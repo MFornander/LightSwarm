759d862a12bb806d892c858806f69b240fe9e495
Mattias Fornander <mfornander@nvidia.com>
2017-04-17 14:37:19 -0600

    Add FPS output

--- src/main.py
+++ src/main.py
@@ -1,27 +1,6 @@
-import time, machine, neopixel, esp
-
-
-def old():
-    led = machine.Pin(2, machine.Pin.OUT)
-    np = neopixel.NeoPixel(machine.Pin(4), 240)
-    on = False
-    offset = 0
-
-    while True:
-        for i in range(0, np.n):
-            np[i] = ((i + offset) % 256, 0, 0)
-
-        np.write()
-        if on:
-            led.low()
-            on = False
-        else:
-            led.high()
-            on = True
-
-        offset = offset +1
-
-        time.sleep_ms(20)
+import utime
+import machine
+import esp
 
 
 if __name__ == '__main__':
@@ -29,28 +8,28 @@ if __name__ == '__main__':
     offset = 0
     on = False
     led = machine.Pin(2, machine.Pin.OUT)
-    led4 = machine.Pin(4, machine.Pin.OUT)
+    gpio4 = machine.Pin(4, machine.Pin.OUT)
 
     machine.freq(160000000)
 
     rgb = bytearray(240*3)
+    oldTime = utime.ticks_ms()
 
     while True:
-        print(i)
         i = i + 1
 
         rgb[offset*3 + 0] = 0
         rgb[offset*3 + 1] = 0
         rgb[offset*3 + 2] = 0
 
-        offset = offset +1
+        offset = offset + 1
         offset = offset % 239
 
         rgb[offset*3 + 0] = 0
         rgb[offset*3 + 1] = 255
         rgb[offset*3 + 2] = 255
 
-        esp.neopixel_write(machine.Pin(4), rgb, True)
+        esp.neopixel_write(gpio4, rgb, True)
 
         if on:
             led.low()
@@ -59,3 +38,6 @@ if __name__ == '__main__':
             led.high()
             on = True
 
+        newTime = utime.ticks_ms()
+        print(1000.0 / utime.ticks_diff(newTime, oldTime))
+        oldTime = newTime
