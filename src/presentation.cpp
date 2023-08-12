481135fa500e2a301f4acce4d9f9ddb062edeb8c
JustinEbert <justin.c.ebert@gmail.com>
2023-08-11 19:21:20 -0600

    Remove file code from Presentation

--- src/presentation.cpp
+++ src/presentation.cpp
@@ -17,30 +17,6 @@ CPresentation::CPresentation()
     }
 }
 
-// TODO(mf): Enable SPIFFS
-/*
-CPresentation::CPresentation(const String& inPresentationPath)
-{
-    m_File = SPIFFS.open(inPresentationPath, "r");
-    if (m_File)
-    {
-        // Read the presentation header information: BPM, URI, StrandCount, StrandOffsets
-        m_BPM = ReadFloat();
-        SkipString(); //Skip the URI
-        m_PresentationStrandCount = ReadByte();
-        m_Index = new SStrandEntry[m_PresentationStrandCount];
-        for (uint8_t theStrand = 0; theStrand < m_PresentationStrandCount; theStrand++)
-        {
-            m_Index[theStrand].m_Offset = ReadULong();
-            m_Index[theStrand].m_ByteCount = ReadUShort();
-            m_Index[theStrand].m_Sequence = nullptr;
-        }
-    }
-    else
-        ERRX("Unable to open the file=%s", inPresentationPath.c_str());
-}
-*/
-
 CPresentation::~CPresentation()
 {
     for (uint8_t theStrand = 0; theStrand < m_PresentationStrandCount; theStrand++)
@@ -79,98 +55,6 @@ CTimeline* CPresentation::CreateTimeline(short inStrandIndex)
     // Wrap the stand index to the number of strands in this presentation
     short   theWrappedIndex = inStrandIndex % m_PresentationStrandCount;
 
-    // If this is from a file and we don't have the data yet then jump to the
-    // location of this strand's timeline and read in the events as a single
-    // buffer.  Cache for later
-    /*if (m_Index[theWrappedIndex].m_Sequence == nullptr && m_File)
-    {
-        m_File.seek(m_Index[theWrappedIndex].m_Offset, SeekSet);
-        m_Index[theWrappedIndex].m_Sequence = new char[m_Index[theWrappedIndex].m_ByteCount];
-        ReadBytes(m_Index[theWrappedIndex].m_Sequence, m_Index[theWrappedIndex].m_ByteCount);
-    }*/
-
     return new CTimeline(m_Index[theWrappedIndex].m_Sequence, m_Index[theWrappedIndex].m_ByteCount);
 }
-
-
-// =============================================================================
-// Private stream reading functions
-// =============================================================================
-/*
-uint8_t CPresentation::ReadByte()
-{
-    uint8_t   theByte;
-
-    m_File.read(&theByte, 1);
-    Serial.println(theByte);
-
-    return theByte;
-}
-
-short CPresentation::ReadShort()
-{
-    short   theShort;
-
-    m_File.read((uint8_t*)&theShort, 2);
-    Serial.println(theShort);
-
-    return theShort;
-}
-
-uint16_t CPresentation::ReadUShort()
-{
-    uint16_t   theUShort;
-
-    m_File.read((uint8_t*)&theUShort, 2);
-    Serial.println(theUShort);
-
-    return theUShort;
-}
-
-long CPresentation::ReadLong()
-{
-    long   theLong;
-
-    m_File.read((uint8_t*)&theLong, 4);
-    Serial.println(theLong);
-
-    return theLong;
-}
-
-uint32_t CPresentation::ReadULong()
-{
-    uint32_t   theULong;
-
-    m_File.read((uint8_t*)&theULong, 4);
-    Serial.println(theULong);
-
-    return theULong;
-}
-
-float CPresentation::ReadFloat()
-{
-    float   theFloat;
-
-    m_File.read((uint8_t*)&theFloat, 4);
-    Serial.println(theFloat);
-
-    return theFloat;
-}
-
-void CPresentation::ReadBytes(char* ioBytes, long inLength)
-{
-    m_File.read((uint8_t*)ioBytes, inLength);
-}
-
-void CPresentation::SkipString()
-{
-    char    theLength;
-    char    theString[255];
-
-    m_File.read((uint8_t*)&theLength, 1);
-    m_File.read((uint8_t*)theString, theLength);
-    theString[theLength] = '\0';
-    Serial.println(theString);
-}
-*/
 }
--- src/presentation.h
+++ src/presentation.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <Arduino.h>
-//#include <SPIFFS.h>
 
 namespace LightSwarm {
 
@@ -22,29 +21,17 @@ typedef SStrandEntry* SIndex;
 class CPresentation final
 {
 protected:
-    //File      m_File;
     float       m_BPM = 0;
     short       m_PresentationStrandCount = 0;
     SIndex      m_Index = nullptr;
 
 public:
     CPresentation();
-    //CPresentation(const String& inPresentationPath);
     ~CPresentation();
 
     static void CreateSequence(uint8_t inEffect, char*& outSequence, uint32_t& outByteCount);
     void        AddStrand(char* inSequence, uint32_t inByteCount);
 
     CTimeline*  CreateTimeline(short inStrandIndex);
-
-private:
-    uint8_t     ReadByte();
-    short       ReadShort();
-    uint16_t    ReadUShort();
-    long        ReadLong();
-    uint32_t    ReadULong();
-    float       ReadFloat();
-    void        ReadBytes(char* ioBytes, long inLength);
-    void        SkipString();
 };
 }
